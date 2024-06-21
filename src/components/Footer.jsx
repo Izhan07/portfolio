5c5788b11fabd34248725c8641c0baba013dddbe
Izhan07 <izhanahmed987@gmail.com>
2024-06-21 09:04:14 -0700

    update Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className='min-[380px]:p-6 pl-3 text-white bg-[#161513]'>
+    <div className='min-[380px]:p-6 pl-3 pr-3 text-white bg-[#161513]'>
         <div className='min-[380px]:w-96 w-80 p-3'>
             <h1 className='font-bold text-4xl p-2'>Izhan</h1>
             <p className='min-[380px]:p-2 min-[380px]:text-lg pl-0 text-sm'>I am a frontend developer from, Pakistan.</p>
         </div>
-        <hr/>
+        <hr className='w-100vw '/>
         <div className='flex md:flex-row flex-col justify-between p-2 '>
             <div className=''>
                 <p>© 2024 Izhan Ahmed. All rights reserved.</p>
